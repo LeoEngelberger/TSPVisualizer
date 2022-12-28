1f93d4b1f863934644d1ef9d9ff03a4b6dccd814
LeoEngelberger <87647723+LeoEngelberger@users.noreply.github.com>
2022-12-28 12:32:32 +0100

    28.12

--- MapGenerator2_0.py
+++ MapGenerator2_0.py
@@ -5,10 +5,10 @@ import sqlite3
 
 
 class Map_Generator():
-    def __init__(self):
+    def __init__(self, lef_border, rig_border, top_border, bot_border):
         plt.figure()
         plt.figure(figsize=(16*5,9*5))
-        self.m = Basemap(resolution='h',projection='merc', llcrnrlat=45.73, urcrnrlat=47.9, llcrnrlon=5.85, urcrnrlon=10.58)
+        self.m = Basemap(resolution='h',projection='merc', llcrnrlat=lef_border, urcrnrlat=rig_border, llcrnrlon=top_border, urcrnrlon=bot_border)
         self.m.drawcoastlines()
         self.m.drawcountries(linewidth=8)
         self.m.fillcontinents((0,0.5,0),(0,0,0.3))
@@ -17,8 +17,11 @@ class Map_Generator():
         lat = 47.36865
         x, y = self.m(long, lat)
         self.m.scatter(x, y, marker='o', color='r', zorder=2, s=150)
-
+        x, y = self.m(5.85, 47.9)
+        self.m.scatter(x, y, marker='o', color='b', zorder=2, s=350)
+        x, y = self.m(10.58, 47.9)
+        self.m.scatter(x, y, marker='o', color='r', zorder=2, s=350)
         plt.savefig('map.png', bbox_inches='tight', pad_inches=0)
 
 
-test = Map_Generator()
+test = Map_Generator(45.73,47.9,5.85,10.58)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from mpl_toolkits.basemap import Basemap
 import sqlite3
 import matplotlib.pyplot as plt
+#self.m = Basemap(resolution='h',projection='merc', llcrnrlat=45.73, urcrnrlat=47.9, llcrnrlon=5.85, urcrnrlon=10.58)
 
 red=(255,0,0)
 blue=(0,0,255)
@@ -16,13 +17,23 @@ class MainGame():
         pygame.init()
         self.screen = pygame.display.set_mode((1920, 1080))
         self.screen_x, self.screen_y = self.screen.get_size()
-
         self.background = pygame.transform.scale(pygame.image.load("map.png"), self.screen.get_size())
+        self.right_most_lat, self.bot_most_long = 47.9, 5.85
+        self.lat_range = 47.9-45.73
+        self.long_range = 10.58-5.85
+        self.pixel_per_lat_degree = self.screen_x / self.lat_range
+        self.pixel_per_long_degree = self.screen_y / self.long_range
         self.screen.blit(self.background, (0,0))
+        self.testVertex2 = Vertex(100,100,blue,5.86,45.73)
+        self.testVertex = Vertex(10,10,red,8.539183,47.36865)
+        self.screen.blit(self.testVertex.image, self.convert_coordinates(self.testVertex))
 
         self.main_loop()
 
-
+    def convert_coordinates(self,vertex):
+        x = (self.right_most_lat-vertex.lat) * self.pixel_per_lat_degree
+        y = (vertex.long-self.bot_most_long ) * self.pixel_per_long_degree
+        return y,x
     def main_loop(self):
         while self.game_running:
             for event in pygame.event.get():
@@ -32,6 +43,14 @@ class MainGame():
                 if event.type == pygame.QUIT:
                     self.game_running = False
             pygame.display.flip()
+
+class Vertex(pygame.sprite.Sprite):
+    def __init__(self,width,height,color,long,lat):
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.long, self.lat = long, lat
+
 
 
 
