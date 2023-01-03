63a64f136184e4fc6699d7f8b319d2170d9152cb
LeoEngelberger <87647723+LeoEngelberger@users.noreply.github.com>
2023-01-04 00:14:18 +0100

    first interactive and path, player class implementation

--- main.py
+++ main.py
@@ -14,18 +14,20 @@ blue = (0, 0, 255)
 green = (0, 255, 0)
 
 pygame.init()
-screen_x, screen_y = 1920,1080
-screen = pygame.display.set_mode((screen_x,screen_y))
+screen_x, screen_y = 1920, 1080
+screen = pygame.display.set_mode((screen_x, screen_y))
 
 
-class Main_Menu():
+class MainMenu:
     def __init__(self):
         self.font = pygame.font.Font('Targa.ttf', 64)
         self.is_enabled = True
-        self.items = [("start game", self.start_game),("set difficulty", self.set_difficulty)]
-        self.home_menu = pygame_menu.Menu("Problem des Handelsreisenden", screen_x, screen_y, theme=themes.THEME_SOLARIZED)
+        self.items = [("start game", self.start_game), ("set difficulty", self.set_difficulty)]
+        self.home_menu = pygame_menu.Menu("Problem des Handelsreisenden", screen_x, screen_y,
+                                          theme=themes.THEME_SOLARIZED)
         self.home_menu.add.button("Play", self.start_game)
-        self.home_menu.add.selector("Difficulty: ", [("Hard", 26), ("Easy",2)], onchange=self.set_difficulty())
+        self.selector = self.home_menu.add.selector("Difficulty: ", [("Hard", 10), ("Easy", 2)],
+                                                    onchange=self.set_difficulty())
 
     def set_difficulty(self):
         pass
@@ -34,14 +36,14 @@ class Main_Menu():
         self.is_enabled = False
 
 
-class GameSession():
-    def __init__(self):
+class GameSession:
+    def __init__(self, number_of_vertices,player):
         self.game_running = True
-        self.amount_of_vertices = 10
+        self.amount_of_vertices = number_of_vertices
 
+        self.player = player
         self.setup_screen()
         self.setup_map()
-        #self.main_loop()
 
     def setup_screen(self):
         self.background = pygame.transform.scale(pygame.image.load("map.png"), screen.get_size())
@@ -62,7 +64,7 @@ class GameSession():
         cursor.execute("SELECT * FROM Location")
         Location = cursor.fetchmany(self.amount_of_vertices)
         for place in Location:
-            temp_vert = Vertex(red, place[1], place[2], place[3])
+            temp_vert = Vertex(red, place[1], place[2], place[3], self.player)
             self.Vertices.add(temp_vert)
             screen.blit(temp_vert.image, self.convert_coordinates(temp_vert))
         cursor.close()
@@ -76,7 +78,7 @@ class GameSession():
         else:
             x = x - 5
         if y == screen_y: y = y - 10
-        vertex.rect.x, vertex.rect.y = (x,y)
+        vertex.rect.x, vertex.rect.y = (x, y)
         return x, y
 
     def update(self, events):
@@ -86,7 +88,7 @@ class GameSession():
                 print(mouse_pos)
                 for i, t_Vertex in enumerate(self.Vertices):
                     if t_Vertex.rect.collidepoint(mouse_pos):
-                        print(t_Vertex.name)
+                        t_Vertex.clicked()
                 # if event is of type quit then
                 # set running bool to false
             if event.type == pygame.QUIT:
@@ -94,12 +96,42 @@ class GameSession():
 
             pygame.display.flip()
 
-
-
-class Main_Game():
+class Path(pygame.sprite.Group):
     def __init__(self):
-        self.main_menu = Main_Menu()
+        super(Path, self).__init__()
+
+
+class Player:
+    def __init__(self):
+        self.path = Path()
+
+    def add_vertex_to_path(self, vertex):
+        self.path.add(vertex)
+
+
+class Vertex(pygame.sprite.Sprite):
+    def __init__(self, color, name, long, lat, player):
+        super(Vertex, self).__init__()
+        self.image = pygame.Surface([20, 20])
+        self.image.fill(color)
+        self.name = name
+        self.rect = self.image.get_rect()
+        self.rect.width, self.rect.height = (30, 30)
+        self.long, self.lat = long, lat
+        self.player=player
+
+    def clicked(self):
+        print(self.name)
+        self.image.fill(green)
+        self.player.add_vertex_to_path(self)
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class MainGame:
+    def __init__(self):
+        self.main_menu = MainMenu()
         self.game_session = None
+        self.player = Player()
         self.main_loop()
 
     def main_loop(self):
@@ -115,22 +147,14 @@ class Main_Game():
 
                 else:
                     if not self.game_session:
-                        self.game_session = GameSession()
+                        amount_of_vertices = self.main_menu.selector.get_value()[0][1]
+                        self.game_session = GameSession(amount_of_vertices, self.player)
                     self.game_session.update(events)
 
                 pygame.display.update()
 
-class Vertex(pygame.sprite.Sprite):
-    def __init__(self, color, name, long, lat):
-        super(Vertex, self).__init__()
-        self.image = pygame.Surface([20, 20])
-        self.image.fill(color)
-        self.name = name
-        self.rect = self.image.get_rect()
-        self.rect.width, self.rect.height = (30,30)
-        self.long, self.lat = long, lat
 
 
 
-main_menu = Main_Menu()
-game = Main_Game()
+#main_menu = MainMenu()
+game = MainGame()
