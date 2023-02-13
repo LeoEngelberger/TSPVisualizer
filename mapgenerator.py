4799c45a3991cc3bb3d2263c38bcfe28c5f48727
LeoEngelberger <87647723+LeoEngelberger@users.noreply.github.com>
2023-02-13 11:53:16 +0100

    tidying up old unused files, combined map generator and got rid of 2_0designation

--- mapgenerator.py
+++ mapgenerator.py
@@ -1,33 +1,24 @@
-
-from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+import numpy as np
 import sqlite3
 
-# Create a new figure
-plt.figure()
-
-# Set the dimensions of the figure
-plt.figure(figsize=(10, 10))
-
-# Set the map projection.
-# You can choose from a variety of map projections.
-# Some examples include: 'aeqd', 'moll', 'robin', 'merc'
-m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180, urcrnrlon=180)
-
-# Draw the coastlines
-m.drawcoastlines()
-
-# Draw country boundaries
-m.drawcountries()
-
-m.shadedrelief()
-
-# Draw lat/lon grid lines every 30 degrees.
-m.drawmeridians(range(-180, 180, 1))
-m.drawparallels(range(-90, 90, 1))
+class Map_Generator():
+    def __init__(self, lef_border, rig_border, top_border, bot_border):
+        plt.figure()
+        plt.figure(figsize=(16*5,9*5))
+        self.m = Basemap(resolution='h',projection='mil', llcrnrlat=lef_border, urcrnrlat=rig_border, llcrnrlon=top_border, urcrnrlon=bot_border, epsg=4306)
 
 
-plt.autoscale()
-# Save the map as an image file
-plt.savefig('map.png',bbox_inches='tight')
+        self.m.arcgisimage(service='World_Shaded_Relief', xpixels=4000)
 
+
+        self.m.drawcountries(linewidth=7)
+
+        self.m.fillcontinents((0,0.4,0,0.3),(0,0,0.3,0.025))
+        self.m.drawmeridians(np.arange(40, 60, 0.5),linewidth=5)
+        self.m.drawparallels(np.arange(0, 20, 1),linewidth=5)
+        plt.savefig('map.png', bbox_inches='tight', pad_inches=0)
+
+
+test = Map_Generator(45.73,47.9,5.85,10.58)
--- test3.py
+++
@@ -1,7 +0,0 @@
-with open("locations_txt.txt",'r',encoding='utf-8') as text_file:
-    text_file = text_file.readlines()
-    for line in range(1,len(text_file)):
-        text_line = text_file[line].split(';')
-        text_line[2]=text_line[2].strip('\n')
-        name,lat,long=text_line
-        print("hello")
